c6132361205d349c373bee4aea706ac887972904
Madushan Sandaruwan <madushansandaru1@gmail.com>
2022-07-08 01:09:44 +0530

    Create RequestConst.java

---
+++ src/main/java/org/example/httpclient/utils/RequestConst.java
@@ -0,0 +1,11 @@
+package org.example.httpclient.utils;
+
+public interface RequestConst {
+
+    public final String get_all_request_url = "http://localhost:8080/api/v1/user/all";
+    public final String get_one_request_url = "http://localhost:8080/api/v1/user/1";
+    public final String post_request_url = "http://localhost:8080/api/v1/user/create";
+    public final String put_request_url = "http://localhost:8080/api/v1/user/update/3";
+    public final String delete_request_url = "http://localhost:8080/api/v1/user/delete/1";
+
+}
